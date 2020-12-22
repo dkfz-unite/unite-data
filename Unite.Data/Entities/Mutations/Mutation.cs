e936a37e9f8faaac94092a9c4eddf17230ddd750
Valiantsin Ulasau <valiantsin.ulasau@dkfz-heidelberg.de>
2020-12-22 15:42:05 +0100

    Adding Code computed property to mutation entity.

--- Unite.Data/Entities/Mutations/Mutation.cs
+++ Unite.Data/Entities/Mutations/Mutation.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Unite.Data.Entities.Extensions;
 using Unite.Data.Entities.Mutations.Enums;
 using Unite.Data.Entities.Samples;
 
@@ -17,9 +18,33 @@ namespace Unite.Data.Entities.Mutations
         public string ReferenceAllele { get; set; }
         public string AlternateAllele { get; set; }
 
+        public string Code => GetCode();
+
         public virtual Gene Gene { get; set; }
         public virtual Contig Contig { get; set; }
 
         public virtual ICollection<SampleMutation> MutationSamples { get; set; }
+
+
+        private string GetCode()
+        {
+            var chr = ChromosomeId != null
+                    ? ChromosomeId.ToDefinitionString()
+                    : Contig.Value;
+
+            var seq = SequenceTypeId.ToDefinitionString();
+
+            var pos = Position.ToString();
+
+            var refA = !string.IsNullOrEmpty(ReferenceAllele)
+                     ? ReferenceAllele
+                     : "-";
+
+            var altA = !string.IsNullOrEmpty(AlternateAllele)
+                     ? AlternateAllele
+                     : "-";
+
+            return $"chr{chr}:{seq}.{pos}{refA}>{altA}";
+        }
     }
 }

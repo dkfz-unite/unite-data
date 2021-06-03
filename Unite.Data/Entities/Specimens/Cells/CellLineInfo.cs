d0840b59c59f61c4854b24cccd2afd78c76f93ae
Valiantsin Ulasau <valiantsin.ulasau@gmail.com>
2021-06-03 16:12:57 +0200

    Renamed cell line public links fields.

--- Unite.Data/Entities/Specimens/Cells/CellLineInfo.cs
+++ Unite.Data/Entities/Specimens/Cells/CellLineInfo.cs
@@ -10,8 +10,9 @@ namespace Unite.Data.Entities.Specimens.Cells
         public string DepositorName { get; set; }
         public string DepositorEstablishment { get; set; }
         public DateTime? EstablishmentDate { get; set; }
-        public string PublicationId { get; set; }
-        public string AtccId { get; set; }
-        public string ExPasyId { get; set; }
+
+        public string PubMedLink { get; set; }
+        public string AtccLink { get; set; }
+        public string ExPasyLink { get; set; }
     }
 }

090c42d229646bc1c856b11cee34b840a91c4da5
Valiantsin Ulasau <valiantsin.ulasau@gmail.com>
2021-07-21 18:36:22 +0200

    Removed dates from file.

--- Unite.Data/Entities/File.cs
+++ Unite.Data/Entities/File.cs
@@ -1,13 +1,9 @@
-﻿using System;
-
-namespace Unite.Data.Entities
+﻿namespace Unite.Data.Entities
 {
     public class File
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Link { get; set; }
-        public DateTime? Created { get; set; }
-        public DateTime? Updated { get; set; }
     }
 }

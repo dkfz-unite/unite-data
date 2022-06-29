f3bda18632bdbbbcec2a8b102d780f26d15f7996
vulasau <valiantsin.ulasau@gmail.com>
2022-06-29 12:08:04 +0200

    Work package description.

--- Unite.Data/Entities/Donors/WorkPackage.cs
+++ Unite.Data/Entities/Donors/WorkPackage.cs
@@ -5,6 +5,7 @@ public class WorkPackage
     public int Id { get; set; }
 
     public string Name { get; set; }
+    public string Description { get; set; }
 
     public virtual ICollection<WorkPackageDonor> WorkPackageDonors { get; set; }
 }

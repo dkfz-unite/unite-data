22282db496468aab4a67b166646d7a0e5386fb93
vulasau <valiantsin.ulasau@gmail.com>
2022-06-30 17:39:54 +0200

    Study description.

--- Unite.Data/Entities/Donors/Study.cs
+++ Unite.Data/Entities/Donors/Study.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections.Generic;
-
-namespace Unite.Data.Entities.Donors;
+﻿namespace Unite.Data.Entities.Donors;
 
 public class Study
 {
     public int Id { get; set; }
 
     public string Name { get; set; }
+    public string Description { get; set; }
 
     public ICollection<StudyDonor> StudyDonors { get; set; }
 }

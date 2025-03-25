6bf665bf77fb15a9bcf7af37a832b053a0d9e3b2
vulasau <valiantsin.ulasau@gmail.com>
2025-03-25 18:33:38 +0100

    Similar variants finding.

--- Unite.Data.Context/Repositories/VariantsRepository.cs
+++ Unite.Data.Context/Repositories/VariantsRepository.cs
@@ -172,4 +172,124 @@ public class VariantsRepository : Repository
             .Select(affectedFeature => affectedFeature.FeatureId)
             .ToArrayAsync();
     }
+
+    public async Task<int[]> GetSimilarVariants<TVAT, TV>(IEnumerable<int> ids, double overlap = 0.9)
+        where TVAT : VariantAffectedTranscript<TV>
+        where TV : Variant
+    {
+        if (typeof(TV) == typeof(Cnv.Variant))
+            return await GetSimilarCnvs(ids, overlap);
+        else if (typeof(TV) == typeof(Sv.Variant))
+            return await GetSimilarSvs(ids, overlap);
+        else
+            return [];
+    }
+
+
+    private async Task<int[]> GetSimilarCnvs(IEnumerable<int> ids, double overlap)
+    {
+        using var dbContext = _dbContextFactory.CreateDbContext();
+
+        var results = new List<int>();
+
+        var targets = await dbContext.Set<Cnv.Variant>()
+            .AsNoTracking()
+            .Where(variant => ids.Contains(variant.Id))
+            .ToArrayAsync();
+
+        foreach (var target in targets)
+        {
+            await dbContext.Set<Cnv.Variant>()
+                .AsNoTracking()
+                .Where(current =>
+                    current.TypeId == target.TypeId &&
+                    current.Loh == target.Loh &&
+                    current.Del == target.Del &&
+                    current.End >= target.Start &&
+                    current.Start <= target.End
+                )
+                .Select(current => new 
+                {
+                    Id = current.Id,
+                    OverlapStart = Math.Max(current.Start, target.Start),
+                    OverlapEnd = Math.Min(current.End, target.End),
+                    CurrentLength = current.End - current.Start,
+                    TargetLength = target.End - target.Start
+                })
+                .Where(current => Math.Min(
+                    (double)(current.OverlapEnd - current.OverlapStart) / current.CurrentLength,
+                    (double)(current.OverlapEnd - current.OverlapStart) / current.TargetLength
+                ) >= overlap)
+                .Select(current => current.Id)
+                .ToArrayAsync();
+        }
+
+        return results.ToArray();
+    }
+
+    private async Task<int[]> GetSimilarSvs(IEnumerable<int> ids, double overlap)
+    {
+        using var dbContext = _dbContextFactory.CreateDbContext();
+
+        var results = new List<int>();
+
+        var targets = await dbContext.Set<Sv.Variant>()
+            .AsNoTracking()
+            .Where(variant => ids.Contains(variant.Id))
+            .ToArrayAsync();
+
+        foreach (var target in targets)
+        {
+            if (target.TypeId == Sv.Enums.SvType.ITX || target.TypeId == Sv.Enums.SvType.CTX)
+            {
+                var distance = 5000;
+                var targetStartMin = Math.Max(0, target.End - distance);
+                var targetStartMax = target.End + distance;
+                var targetEndMin = Math.Max(0, target.OtherStart - distance);
+                var targetEndMax = target.OtherStart + distance;
+
+                results.AddRange(await dbContext.Set<Sv.Variant>()
+                    .AsNoTracking()
+                    .Where(current =>
+                        current.TypeId == target.TypeId &&
+                        current.Inverted == target.Inverted &&
+                        current.ChromosomeId == target.ChromosomeId &&
+                        current.OtherChromosomeId == target.OtherChromosomeId &&
+                        current.End >= targetStartMin &&
+                        current.End <= targetStartMax &&
+                        current.OtherStart >= targetEndMin &&
+                        current.OtherStart <= targetEndMax
+                    )
+                    .Select(current => current.Id)
+                    .ToArrayAsync());
+            }
+            else
+            {
+                results.AddRange(await dbContext.Set<Sv.Variant>()
+                    .AsNoTracking()
+                    .Where(current =>
+                        current.TypeId == target.TypeId &&
+                        current.Inverted == target.Inverted &&
+                        current.End >= target.Start &&
+                        current.Start <= target.End
+                    )
+                    .Select(current => new 
+                    {
+                        Id = current.Id,
+                        OverlapStart = Math.Max(current.End, target.End),
+                        OverlapEnd = Math.Min(current.OtherStart, target.OtherStart),
+                        CurrentLength = current.OtherStart - current.End,
+                        TargetLength = target.OtherStart - target.End
+                    })
+                    .Where(current => Math.Min(
+                        (double)(current.OverlapEnd - current.OverlapStart) / current.CurrentLength,
+                        (double)(current.OverlapEnd - current.OverlapStart) / current.TargetLength
+                    ) >= overlap)
+                    .Select(current => current.Id)
+                    .ToArrayAsync());
+            }
+        }
+
+        return results.ToArray();
+    }
 }

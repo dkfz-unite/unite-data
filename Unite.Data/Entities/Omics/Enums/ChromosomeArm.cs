3f13fcbf7a3f27513e1e20e1eb097ce6f6345c14
SemionV <semion.varennik@gmail.com>
2026-01-22 16:59:28 +0100

    Remove redundant comment

--- Unite.Data/Entities/Omics/Enums/ChromosomeArm.cs
+++ Unite.Data/Entities/Omics/Enums/ChromosomeArm.cs
@@ -4,9 +4,6 @@ namespace Unite.Data.Entities.Omics.Enums;
 
 public enum ChromosomeArm
 {
-    /// <summary>
-    /// Chromosome 1.
-    /// </summary>
     [EnumMember(Value = "Short")]
     Short,
     [EnumMember(Value = "Long")]

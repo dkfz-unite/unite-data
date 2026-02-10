d598fbf93b0a785ca1170ae19832294edb69498e
SemionV <semion.varennik@gmail.com>
2026-02-10 16:59:55 +0100

    Rename CnvProfile columns

--- Unite.Data/Entities/Omics/Analysis/CnvProfile.cs
+++ Unite.Data/Entities/Omics/Analysis/CnvProfile.cs
@@ -1,14 +1,21 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Unite.Data.Entities.Omics.Enums;
 
 namespace Unite.Data.Entities.Omics.Analysis;
 
 public record CnvProfile: Base.Entity
 {
+    [Column("sample_id")]
     public int SampleId { get; set; }
     public Sample Sample { get; set; }
+    [Column("chromosome")]
     public Chromosome Chromosome { get; set; }
+    [Column("chromosome_arm")]
     public ChromosomeArm ChromosomeArm { get; set; }
+    [Column("gain")]
     public float Gain { get; set; }
+    [Column("loss")]
     public float Loss { get; set; }
+    [Column("neutral")]
     public float Neutral { get; set; }
 }
